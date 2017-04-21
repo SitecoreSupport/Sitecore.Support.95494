daf1605f58235055774aac2e494f02e183ff2e54
Viktor Kucherov <vkucherov@live.ru>
2017-04-21 13:44:34 +0800

    no message

---
+++ src/Sitecore.Support.95494/Pipelines/PreprocessRequest/ProtectItemWebApiRequest.cs
@@ -0,0 +1,24 @@
+﻿using Sitecore.Pipelines.PreprocessRequest;
+using System;
+using System.Reflection;
+
+namespace Sitecore.Support.Pipelines.PreprocessRequest
+{
+  public class ProtectItemWebApiRequest : PreprocessRequestProcessor
+  {
+    // Methods
+    public override void Process(PreprocessRequestArgs args)
+    {
+      if (args.Context.Request.Url.LocalPath.StartsWith("/-/item/v1/", StringComparison.OrdinalIgnoreCase))
+      {
+        try
+        {
+          args.Context.Request.GetType().GetMethod("get_Form", BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Static | BindingFlags.Instance).Invoke(args.Context.Request, new object[0]);
+        }
+        catch (Exception)
+        {
+        }
+      }
+    }
+  }
+}
---
+++ src/Sitecore.Support.95494/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.95494")]
+[assembly: AssemblyProduct("Sitecore.Support.95494")]
+[assembly: ComVisible(false)]
